d2ed4ea2096f4b729159368b0fb39f0f20d16056
zhunan123 <zhunan@bu.edu>
2023-07-15 14:16:40 -0700

    added docker set up and nodejs app demo

---
+++ source/_test_/test.spec.js
@@ -0,0 +1,7 @@
+describe("Testing our nodeJS app", () => {
+    it("Testing using Github Actions", () => {
+        // exoecting 3 functions and receiving 3
+        //  this test will pass unless the value change
+        expect(3).toBe(3);
+    });
+});
